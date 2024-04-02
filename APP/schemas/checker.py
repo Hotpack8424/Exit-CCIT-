30d6a401438164fe23f77fa5094b4f64e5fc8874
ZINH00 <161221255+ZINH00@users.noreply.github.com>
2024-04-02 22:59:50 +0900

    Add files via upload

--- APP/schemas/checker.py
+++ APP/schemas/checker.py
@@ -3,11 +3,14 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
 class SiteCheckRequest(BaseModel):
     url: str
 
 class SiteCheckResponse(BaseModel):
     url: str
     blocked: bool
-    
+
+class RedirectData(BaseModel):
+    url: str
+    redirectUrl: str
+    statusCode: int
--- APP/utils/crawler.py
+++ APP/utils/crawler.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
-from bs4 import BeautifulSoup
 import time
 
 router = APIRouter()
